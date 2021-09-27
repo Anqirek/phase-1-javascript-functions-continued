aeb8dd3016598f9e01a60f5a5453c63ddce13490
Anthony Hill <hillganthony@outlook.com>
2021-09-27 15:02:12 -0400

    commit tree 5c2993f5849e256eafbddfd6e7eb87081de0b6df

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+//  function saturdayFun(text) {
+//   console.log(`This Saturday, I want to ${text}!`)   
+// }
+
+function saturdayFun(hobby="roller-skate"){
+    console.log("This Saturday, I want to ${hobby}")
+    return `This Saturday, I want to ${hobby}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function () {
+    let activity = "go to the office";
+    console.log("This Monday, I will ${activity}.");
+    return `This Monday, I will ${activity}.`; 
+}
+mondayWork();
+mondayWork("This Monday, I will work from home.");
+
+
+
+
+
